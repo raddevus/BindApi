9f25168cc5cbaa43ab9712cb3012b067aad962fa
raddevus <1radeutsch1@gmail.com>
2024-06-09 15:42:29 -0400

    Removed unused code, added RegisterUser2 (FromForm) which required the new DisableAntiforgery().

--- Program.cs
+++ Program.cs
@@ -26,34 +26,15 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+app.MapPost("/RegisterUser1", IResult ([FromQuery] string uuid) =>   {
+    return Results.Ok(new { Message = $"QUERY - You sent in uuid: {uuid}" });
 })
-.WithName("GetWeatherForecast")
 .WithOpenApi();
 
-app.MapPost("/RegisterUser1", IResult ([FromQuery] string uuid) =>   {
-    // return new JsonResult(new { result = $"You sent me : {uuid}" });
-    return Results.Ok(new { Message = $"You sent in uuid: {uuid}" });
-});
+app.MapPost("/RegisterUser2", IResult ([FromForm] string uuid) =>   {
+    return Results.Ok(new { Message = $"FORM - You sent in uuid: {uuid}" });
+})
+.DisableAntiforgery()
+.WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
