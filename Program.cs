21b7be3ddecea9d81c63e06967b10f748071fcf8
raddevus <1radeutsch1@gmail.com>
2024-06-09 12:56:55 -0400

    Got first post working after adding CORS policy so the webapi can be posted to with Fetch

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options => options.AddDefaultPolicy(builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+}));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -6,13 +14,15 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseCors();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
 
 app.UseHttpsRedirection();
 
@@ -35,6 +45,10 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
+
+app.MapPost("/RegisterUser1", ActionResult ([FromQuery] string uuid) =>   {
+    return new JsonResult(new { result = $"You sent me : {uuid}" });
+});
 
 app.Run();
 
